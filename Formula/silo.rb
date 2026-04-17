6fb5faedbf5a03edd71df93ee3ed540c9fe19f55
Dan Milne <d@nmilne.com>
2026-04-17 10:53:10 +1000

    Update

--- Formula/silo.rb
+++ Formula/silo.rb
@@ -1,28 +1,28 @@
 class Silo < Formula
   desc "Seafile-compatible server and client in one binary"
   homepage "https://github.com/dkam/silo"
-  version "0.3.1"
+  version "0.3.2"
   license "AGPL-3.0-only"
 
   on_macos do
     on_arm do
-      url "https://github.com/dkam/silo/releases/download/v0.3.1/silo-v0.3.1-darwin-arm64.tar.gz"
-      sha256 "701bb0aeb81725660f3624d986b8db0d7b6959842c725f5fd8836a1f36417201"
+      url "https://github.com/dkam/silo/releases/download/v0.3.2/silo-v0.3.2-darwin-arm64.tar.gz"
+      sha256 "834d3a7e925e8ff245f23f9149661baa0562d215b1239d6e07cd3891b0a9cc53"
     end
     on_intel do
-      url "https://github.com/dkam/silo/releases/download/v0.3.1/silo-v0.3.1-darwin-amd64.tar.gz"
-      sha256 "75cb9a5ce36d8acbcdb070a83e6171efbc674dc2e18a3182b745a4dd49e0ce62"
+      url "https://github.com/dkam/silo/releases/download/v0.3.2/silo-v0.3.2-darwin-amd64.tar.gz"
+      sha256 "59aa2b30e3761cfd46f4b71e22c06f1f26080ebb2d5e06bc227a82d252840536"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/dkam/silo/releases/download/v0.3.1/silo-v0.3.1-linux-arm64.tar.gz"
-      sha256 "83fd9eb8c6664b4f64e9c3d38d223159a8ce4b3dfdc9e913ff4f3f76051e2bb0"
+      url "https://github.com/dkam/silo/releases/download/v0.3.2/silo-v0.3.2-linux-arm64.tar.gz"
+      sha256 "e2b0892c1a4b404e09fbc046b7e3c10d9dae3f66e60fd0fb3778db07f566e8a3"
     end
     on_intel do
-      url "https://github.com/dkam/silo/releases/download/v0.3.1/silo-v0.3.1-linux-amd64.tar.gz"
-      sha256 "3742d5051e84c5971272db87580c74fdc03d5dc38c764ae3b8d99c57e22fcfcd"
+      url "https://github.com/dkam/silo/releases/download/v0.3.2/silo-v0.3.2-linux-amd64.tar.gz"
+      sha256 "c92c4e83ad7ed960c0cb19a81c35f64b600e262d712ff344ef03ca8c68bf16fd"
     end
   end
 
